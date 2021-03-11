030eff46ec103825445c38b47ae76e87879be50f
Gor <7495500@gmail.com>
2021-03-11 22:57:29 +0300

    Add divise and public controller

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'omniauth'
+gem 'omniauth-canvas'
+gem 'devise'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  { username: 'sarah',    email: 'sarah@example.com',     password: 'Password1' },
+  { username: 'emily',    email: 'emily@example.com',     password: 'Password1' },
+  { username: 'melanie',  email: 'melanie@example.com',   password: 'Password1' },
+  { username: 'caroline', email: 'caroline@example.com',  password: 'Password1' },
+  { username: 'bridget',  email: 'bridget@example.com',   password: 'Password1' }
+])
